9ca830f42ac60cdb3fc87760174a83c5969d0f34
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-01-16 17:00:43 +0300

    add open register form

--- app/src/main/java/com/example/choiceitsamsungschool/MainActivity.java
+++ app/src/main/java/com/example/choiceitsamsungschool/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.choiceitsamsungschool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton create_account = findViewById(R.id.create_account);
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        MainActivity.this, R.style.BottomSheetDialogTheme
+                );
+                View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(
+                        R.layout.bottom_sheet_create_account_activity,
+                        (LinearLayout) findViewById(R.id.bottomSheetCreateAccount)
+                );
+                bottomSheetView.findViewById(R.id.bottomSheetCreateAccountButton).setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "Create", Toast.LENGTH_SHORT).show();
+                                bottomSheetDialog.dismiss();
+                            }
+                        }
+                );
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 }
