bc017b4a2efc5f30724074cc32d999f4bdb3f54b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-02-12 19:01:15 +0300

    add friends list to database

---
+++ app/src/main/java/com/example/choiceitsamsungschool/db/FriendDao.java
@@ -0,0 +1,29 @@
+package com.example.choiceitsamsungschool.db;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+
+@Dao
+public interface FriendDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addFriend(Friend friend);
+
+    @Query("select * from Friend")
+    public List<Friend> getAllFriend();
+
+    @Query("select * from Friend where id = :friendId")
+    public List<Friend> getFriend(long friendId);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateFriend(Friend friend);
+
+    @Query("delete from Friend")
+    void removeAllFriends();
+}
