8fda83a4ac65ede02d95473e71cec496f3edcc5d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-04-15 22:24:56 +0300

    update onCreateView

--- app/src/main/java/com/example/choiceitsamsungschool/main_page/HomePage.java
+++ app/src/main/java/com/example/choiceitsamsungschool/main_page/HomePage.java
@@ -87,200 +87,196 @@ public class HomePage extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (page == null) {
-            Context context = getContext();
+        Context context = getContext();
 
-            manager = (InputMethodManager) MainActivity.get().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        manager = (InputMethodManager) MainActivity.get().getSystemService(Activity.INPUT_METHOD_SERVICE);
 
-            home_page = inflater.inflate(R.layout.home_page, container, false);
-            this.inflater = inflater;
+        home_page = inflater.inflate(R.layout.home_page, container, false);
+        HomePage.inflater = inflater;
 
-            apiServer = APIServer.getSingletonAPIServer();
-            apiServer.setHomePage(this);
-            apiServer.loadUserData();
+        apiServer = APIServer.getSingletonAPIServer();
+        apiServer.setHomePage(this);
+        apiServer.loadUserData();
 
-            toolbar = home_page.findViewById(R.id.home_tool_bar);
-            toolbar.setNavigationOnClickListener(v -> changeState());
+        toolbar = home_page.findViewById(R.id.home_tool_bar);
+        toolbar.setNavigationOnClickListener(v -> changeState());
 
-            icon_collapsed_date = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
-            icon_expanded_date = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
+        icon_collapsed_date = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
+        icon_expanded_date = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
 
-            icon_collapsed_active = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
-            icon_expanded_active = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
+        icon_collapsed_active = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
+        icon_expanded_active = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
 
-            icon_collapsed_most_popular = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
-            icon_expanded_most_popular = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
+        icon_collapsed_most_popular = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_collapsed_to_expanded);
+        icon_expanded_most_popular = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_checkable_expandcollapse_expanded_to_collapsed);
 
-            menu = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_menu_animated);
-            close = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_close_animated);
+        menu = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_menu_animated);
+        close = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_close_animated);
 
-            LinearLayout contentLayout = home_page.findViewById(R.id.home_page_front);
+        LinearLayout contentLayout = home_page.findViewById(R.id.home_page_front);
 
-            sheetBehavior = BottomSheetBehavior.from(contentLayout);
-            sheetBehavior.setFitToContents(false);
-            sheetBehavior.setHideable(false);
-            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        sheetBehavior = BottomSheetBehavior.from(contentLayout);
+        sheetBehavior.setFitToContents(false);
+        sheetBehavior.setHideable(false);
+        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-            friends_group = home_page.findViewById(R.id.home_page_friends_chips_group);
-            AppDatabase appDatabase = AppDatabase.getDatabase(context);
-            friends = appDatabase.friendDao().getAllFriend();
+        friends_group = home_page.findViewById(R.id.home_page_friends_chips_group);
+        AppDatabase appDatabase = AppDatabase.getDatabase(context);
+        friends = appDatabase.friendDao().getAllFriend();
 
-            if (friends.size() != 0) {
-                friends_group.removeAllViews();
-                for (int i = 0; i < friends.size(); i++) {
-                    Chip friend = new Chip(context);
-                    friend.setText(friends.get(i).first_name + " " + friends.get(i).second_name);
-                    ChipDrawable chipDrawable = ChipDrawable.createFromAttributes(
-                            context,
-                            null,
-                            0,
-                            R.style.ChipStyle
-                    );
-                    friend.setChipDrawable(chipDrawable);
-                    friends_group.addView(friend);
-                    friends_list.add(friends.get(i));
-                }
+        if (friends.size() != 0) {
+            friends_group.removeAllViews();
+            for (int i = 0; i < friends.size(); i++) {
+                Chip friend = new Chip(context);
+                friend.setText(friends.get(i).first_name + " " + friends.get(i).second_name);
+                ChipDrawable chipDrawable = ChipDrawable.createFromAttributes(
+                        context,
+                        null,
+                        0,
+                        R.style.ChipStyle
+                );
+                friend.setChipDrawable(chipDrawable);
+                friends_group.addView(friend);
+                friends_list.add(friends.get(i));
             }
-
-            MaterialButton show_all_friends = home_page.findViewById(R.id.home_page_show_all_friends);
-            show_all_friends.setOnClickListener(new View.OnClickListener() {
-                private boolean show_all = true;
-
-                @Override
-                public void onClick(View v) {
-                    ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) friends_group.getLayoutParams();
-                    if (show_all) {
-                        lp.matchConstraintMaxHeight = 0;
-                        friends_group.setLayoutParams(lp);
-                        show_all_friends.setText("Спрятать");
-                        show_all = false;
-                    } else {
-                        lp.matchConstraintMaxHeight = 170;
-                        friends_group.setLayoutParams(lp);
-                        show_all_friends.setText("Показать все");
-                        show_all = true;
-                    }
-                }
-            });
-
-            is_increasing_date_button = home_page.findViewById(R.id.home_page_check_box_date_button);
-            is_increasing_date_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (is_increasing_date) {
-                        is_increasing_date_button.setIcon(icon_expanded_date);
-                        is_increasing_date_button.setText(R.string.decreasing);
-                        icon_expanded_date.start();
-                        is_increasing_date = false;
-                    } else {
-                        is_increasing_date_button.setIcon(icon_collapsed_date);
-                        is_increasing_date_button.setText(R.string.increasing);
-                        icon_collapsed_date.start();
-                        is_increasing_date = true;
-                    }
-                }
-            });
-
-            is_increasing_active_button = home_page.findViewById(R.id.home_page_check_box_active_button);
-            is_increasing_active_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (is_increasing_active) {
-                        is_increasing_active_button.setIcon(icon_expanded_active);
-                        is_increasing_active_button.setText(R.string.decreasing);
-                        icon_expanded_active.start();
-                        is_increasing_active = false;
-                    } else {
-                        is_increasing_active_button.setIcon(icon_collapsed_active);
-                        is_increasing_active_button.setText(R.string.increasing);
-                        icon_collapsed_active.start();
-                        is_increasing_active = true;
-                    }
-                }
-            });
-
-            is_increasing_most_popular_button = home_page.findViewById(R.id.home_page_check_box_most_popular_button);
-            is_increasing_most_popular_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (is_increasing_most_popular) {
-                        is_increasing_most_popular_button.setIcon(icon_expanded_most_popular);
-                        is_increasing_most_popular_button.setText(R.string.decreasing);
-                        icon_expanded_most_popular.start();
-                        is_increasing_most_popular = false;
-                    } else {
-                        is_increasing_most_popular_button.setIcon(icon_collapsed_most_popular);
-                        is_increasing_most_popular_button.setText(R.string.increasing);
-                        icon_collapsed_most_popular.start();
-                        is_increasing_most_popular = true;
-                    }
-                }
-            });
-
-            from = home_page.findViewById(R.id.home_page_number_picker_from);
-            from.setMinValue(1);
-            from.setMaxValue(15);
-            from.setValue(1);
-            from.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    to.setMinValue(newVal);
-                }
-            });
-
-            to = home_page.findViewById(R.id.home_page_number_picker_to);
-            to.setMinValue(1);
-            to.setMaxValue(15);
-            to.setValue(15);
-            to.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    from.setMaxValue(newVal);
-                }
-            });
-
-            MaterialButton reset_all = home_page.findViewById(R.id.home_page_reset_all);
-            reset_all.setOnClickListener(v -> resetAll());
-
-            MaterialButton apply = home_page.findViewById(R.id.home_page_apply);
-            apply.setOnClickListener(v -> saveChanges());
-
-            parent_surveys = (ViewGroup) home_page.findViewById(R.id.home_page_content_layout);
-            parent_surveys.removeAllViews();
-
-            if (surveys_list.size() != 0) {
-                parent_surveys.removeAllViews();
-                for (int i = 0; i < surveys_list.size(); i++) {
-                    SurveyCard surveyCard = new SurveyCard(
-                            context,
-                            surveys_list.get(i).survey_id,
-                            InternalStorage.getInternalStorage().load(surveys_list.get(i).survey_id, InternalStorage.SURVEY_TITLE_IMAGE),
-                            inflater,
-                            this);
-                    parent_surveys.addView(surveyCard.getPage());
-                }
-            }
-
-            check_box_date = home_page.findViewById(R.id.home_page_check_box_date);
-            check_box_active = home_page.findViewById(R.id.home_page_check_box_active);
-            check_box_most_popular = home_page.findViewById(R.id.home_page_check_box_most_popular);
-
-            friend_name_field = home_page.findViewById(R.id.home_page_friend_name_input);
-            friend_name_field.addTextChangedListener(new TextInputWatcher(this));
-
-            SwipeRefreshLayout swipeRefreshLayout = home_page.findViewById(R.id.home_page_refresh);
-            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-                @Override
-                public void onRefresh() {
-                    refresh();
-                }
-            });
-
-            page = this;
-            return home_page;
-        } else {
-            return page.getHome_page();
         }
+
+        MaterialButton show_all_friends = home_page.findViewById(R.id.home_page_show_all_friends);
+        show_all_friends.setOnClickListener(new View.OnClickListener() {
+            private boolean show_all = true;
+
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) friends_group.getLayoutParams();
+                if (show_all) {
+                    lp.matchConstraintMaxHeight = 0;
+                    friends_group.setLayoutParams(lp);
+                    show_all_friends.setText("Спрятать");
+                    show_all = false;
+                } else {
+                    lp.matchConstraintMaxHeight = 170;
+                    friends_group.setLayoutParams(lp);
+                    show_all_friends.setText("Показать все");
+                    show_all = true;
+                }
+            }
+        });
+
+        is_increasing_date_button = home_page.findViewById(R.id.home_page_check_box_date_button);
+        is_increasing_date_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (is_increasing_date) {
+                    is_increasing_date_button.setIcon(icon_expanded_date);
+                    is_increasing_date_button.setText(R.string.decreasing);
+                    icon_expanded_date.start();
+                    is_increasing_date = false;
+                } else {
+                    is_increasing_date_button.setIcon(icon_collapsed_date);
+                    is_increasing_date_button.setText(R.string.increasing);
+                    icon_collapsed_date.start();
+                    is_increasing_date = true;
+                }
+            }
+        });
+
+        is_increasing_active_button = home_page.findViewById(R.id.home_page_check_box_active_button);
+        is_increasing_active_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (is_increasing_active) {
+                    is_increasing_active_button.setIcon(icon_expanded_active);
+                    is_increasing_active_button.setText(R.string.decreasing);
+                    icon_expanded_active.start();
+                    is_increasing_active = false;
+                } else {
+                    is_increasing_active_button.setIcon(icon_collapsed_active);
+                    is_increasing_active_button.setText(R.string.increasing);
+                    icon_collapsed_active.start();
+                    is_increasing_active = true;
+                }
+            }
+        });
+
+        is_increasing_most_popular_button = home_page.findViewById(R.id.home_page_check_box_most_popular_button);
+        is_increasing_most_popular_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (is_increasing_most_popular) {
+                    is_increasing_most_popular_button.setIcon(icon_expanded_most_popular);
+                    is_increasing_most_popular_button.setText(R.string.decreasing);
+                    icon_expanded_most_popular.start();
+                    is_increasing_most_popular = false;
+                } else {
+                    is_increasing_most_popular_button.setIcon(icon_collapsed_most_popular);
+                    is_increasing_most_popular_button.setText(R.string.increasing);
+                    icon_collapsed_most_popular.start();
+                    is_increasing_most_popular = true;
+                }
+            }
+        });
+
+        from = home_page.findViewById(R.id.home_page_number_picker_from);
+        from.setMinValue(1);
+        from.setMaxValue(15);
+        from.setValue(1);
+        from.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                to.setMinValue(newVal);
+            }
+        });
+
+        to = home_page.findViewById(R.id.home_page_number_picker_to);
+        to.setMinValue(1);
+        to.setMaxValue(15);
+        to.setValue(15);
+        to.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                from.setMaxValue(newVal);
+            }
+        });
+
+        MaterialButton reset_all = home_page.findViewById(R.id.home_page_reset_all);
+        reset_all.setOnClickListener(v -> resetAll());
+
+        MaterialButton apply = home_page.findViewById(R.id.home_page_apply);
+        apply.setOnClickListener(v -> saveChanges());
+
+        parent_surveys = (ViewGroup) home_page.findViewById(R.id.home_page_content_layout);
+        parent_surveys.removeAllViews();
+
+        if (surveys_list.size() != 0) {
+            parent_surveys.removeAllViews();
+            for (int i = 0; i < surveys_list.size(); i++) {
+                SurveyCard surveyCard = new SurveyCard(
+                        context,
+                        surveys_list.get(i).survey_id,
+                        InternalStorage.getInternalStorage().load(surveys_list.get(i).survey_id, InternalStorage.SURVEY_TITLE_IMAGE),
+                        inflater,
+                        this);
+                parent_surveys.addView(surveyCard.getPage());
+            }
+        }
+
+        check_box_date = home_page.findViewById(R.id.home_page_check_box_date);
+        check_box_active = home_page.findViewById(R.id.home_page_check_box_active);
+        check_box_most_popular = home_page.findViewById(R.id.home_page_check_box_most_popular);
+
+        friend_name_field = home_page.findViewById(R.id.home_page_friend_name_input);
+        friend_name_field.addTextChangedListener(new TextInputWatcher(this));
+
+        SwipeRefreshLayout swipeRefreshLayout = home_page.findViewById(R.id.home_page_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+
+        page = this;
+        return home_page;
     }
 
     @SuppressLint("SetTextI18n")
